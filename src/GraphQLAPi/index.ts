db5ac2e168fd3b08b55533bcd848e1a9ea9749d3
Carlos Gonzalez <carlosfabian512@gmail.com>
2023-07-31 14:39:49 -0600

    Task:7: set up graphQL

---
+++ src/GraphQLAPi/index.ts
@@ -0,0 +1,33 @@
+// npm install @apollo/server express graphql cors body-parser
+import express, { Express } from 'express';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import http from 'http';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import schema from './schema';
+
+
+export async function InitializeApi(serverPort:Number) {
+
+  const app: Express = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs: schema.typeDefs,
+    resolvers: schema.resolvers,
+    introspection: process.env.NODE_ENV !== 'production',
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+  await server.start();
+  app.use(
+    '/',
+    cors<cors.CorsRequest>(),
+    bodyParser.json(),
+    expressMiddleware(server),
+  );
+
+  await new Promise<void>((resolve) => httpServer.listen({ port: serverPort }, resolve));
+  console.log(`🚀 Server ready at port ${serverPort}`);
+
+}
---
+++ src/GraphQLAPi/schema.ts
@@ -0,0 +1,18 @@
+
+const typeDefs = `#graphql
+type Query {
+    getVersion: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    getVersion: () => `v1`,
+  },
+};
+
+
+export default {
+    typeDefs,
+    resolvers
+};
